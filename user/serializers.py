b3aa0b2c0cffd1d162617ee5eddec8a81c5b11df
halyna <1208petrova@gmail.com>
2023-11-22 12:42:55 +0200

    added the number of borrowings to the user endpoint

--- user/serializers.py
+++ user/serializers.py
@@ -1,11 +1,16 @@
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    num_borrowings = serializers.SerializerMethodField()
+
+    def get_num_borrowings(self, obj):
+        return obj.borrowings.count()
+
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff", "num_borrowings")
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
