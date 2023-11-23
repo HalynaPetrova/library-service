3488f29947ba5fb44d8c34e74780495149cdbc97
halyna <1208petrova@gmail.com>
2023-11-23 10:15:51 +0200

    added model Borrowing in borrowing/models.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from book.models import Book
+
+
+class Borrowing(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings")
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return = models.DateField()
+    actual_return = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+
+    @property
+    def is_active(self):
+        return self.actual_return is None
