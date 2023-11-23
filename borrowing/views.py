b3391102345bac2c48ee4e4eec1bc45cdee39cdb
halyna <1208petrova@gmail.com>
2023-11-23 10:24:04 +0200

    created views for borrowing endpoints

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingDetailSerializer, BorrowingCreateSerializer, BorrowingReturnSerializer,
+)
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        if self.action == "return_borrowing":
+            return BorrowingReturnSerializer
+        return BorrowingSerializer
+
+    @action(
+        methods=["PATCH"],
+        detail=True,
+        url_path="return",
+
+    )
+    def return_borrowing(self, request, pk=None):
+
+        serializer = self.get_serializer(
+            instance=self.get_object(),
+            data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,
+                        status=status.HTTP_200_OK)
+
