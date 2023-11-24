810ce32a165314ff9838ea2b02ca012570617734
halyna <1208petrova@gmail.com>
2023-11-24 15:25:01 +0200

    fixed borrowing/models.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,6 @@
+import calendar
+from datetime import date, timedelta
+
 from django.conf import settings
 from django.db import models
 
@@ -7,10 +10,15 @@ from book.models import Book
 class Borrowing(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings")
     borrow_date = models.DateField(auto_now_add=True)
-    expected_return = models.DateField()
     actual_return = models.DateField(blank=True, null=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
 
     @property
     def is_active(self):
         return self.actual_return is None
+
+    def expected_return(self) -> date:
+        expected_return = self.borrow_date
+        days_in_month = calendar.monthrange(expected_return.year, expected_return.month)[1]
+        expected_return += timedelta(days=days_in_month)
+        return expected_return
