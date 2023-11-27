fc86364fadedc6ae89fb16b491562fcee3fc159a
halyna <1208petrova@gmail.com>
2023-11-27 17:20:33 +0200

    added filtering for borrowing by book, user, is_active and borrow_date

--- borrowing/filters.py
+++ borrowing/filters.py
@@ -1,6 +1,6 @@
-from django_filters import rest_framework as filters
+import django_filters
+from django_filters import rest_framework as filters, BooleanFilter
 
-from book.models import Book
 from borrowing.models import Borrowing
 
 
@@ -9,15 +9,19 @@ class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
 
 
 class BorrowingFilter(filters.FilterSet):
-    borrow_date = filters.DateTimeFromToRangeFilter()
-    actual_return = filters.DateTimeFromToRangeFilter()
-    book = CharFilterInFilter(field_name="book", lookup_expr="in")
     user = CharFilterInFilter(field_name="user", lookup_expr="in")
+    book__title = django_filters.CharFilter(lookup_expr="icontains")
+    is_active = BooleanFilter()
+    borrow_date__lt = django_filters.DateFilter(field_name='borrow_date', lookup_expr='lt')
+    borrow_date__gt = django_filters.DateFilter(field_name='borrow_date', lookup_expr='gt')
 
     class Meta:
         model = Borrowing
         fields = [
-            "borrow_date",
-            "actual_return",
+            "book",
             "user",
+            "is_active",
+            "borrow_date",
+            "borrow_date__lt",
+            "borrow_date__gt",
         ]
