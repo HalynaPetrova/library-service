5a54813286360f40d9e414b1c61278e80bd8a74f
halyna <1208petrova@gmail.com>
2023-12-01 09:37:37 +0200

    added custom permissions for endpoint borrowing

---
+++ borrowing/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminOrIsOwner(BasePermission):
+    edit_methods = ("PUT", "PATCH", "DELETE",)
+
+    def has_permission(self, request, view):
+        return bool(
+            (
+                request.method in SAFE_METHODS
+                or request.method not in self.edit_methods
+                and request.user
+                and request.user.is_authenticated
+            )
+            or (request.user and request.user.is_staff)
+        )
