fff3cdd020548e33835e7528e630b758b82a9525
halyna <1208petrova@gmail.com>
2023-12-02 20:14:31 +0200

    fixed file money_to_pay.py

--- payment/money_to_pay.py
+++ payment/money_to_pay.py
@@ -2,10 +2,8 @@ import datetime
 
 
 def money_to_pay(borrowing):
-    borrowing_days = (datetime.date.today() - borrowing.borrow_date).days
-    if borrowing_days == 0:
-        price_in_cents = int(borrowing.book.daily_fee * 100)
-    elif borrowing.expected_return < datetime.date.today():
+    borrowing_days = (datetime.date.today() - borrowing.borrow_date).days + 1
+    if borrowing.expected_return < datetime.date.today():
         price_in_cents = int(borrowing.book.daily_fee * borrowing_days * 101)
     else:
         price_in_cents = int(borrowing.book.daily_fee * borrowing_days * 100)
